efcc24a66f42bc05fc7c939ebfc100bc0100635b
kumarisumirangupta <kumarisumirangupta1611@gmail.com>
2025-12-02 12:02:59 +0530

    Practice for Priority Queue

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,30 @@ public class Main {
         System.out.println(queue.remove());
         System.out.println(queue.poll());
         System.out.println(queue.element()); //Returns exception if queue is empty */
+
+        //Priority Queue
+        /*PriorityQueue<Integer> pq = new PriorityQueue<>( new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o2 > o1) {
+                    return 1;
+                } else if (o2.equals(o1)) {
+                    return 0;
+                } return -1;
+            }
+        });*/
+
+        /*PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2-o1);
+        pq.offer(9);
+        pq.offer(12);
+        pq.offer(8);
+        pq.offer(7);
+        pq.offer(20);
+
+        Iterator<Integer> iteratorPq = pq.iterator();
+        while(iteratorPq.hasNext()) {
+            //This will print level order traversal of heap
+            System.out.println(iteratorPq.next());
+        }*/
     }
 }
