6e426e297c8e4e3aa60d29d96f36028949b0560f
kumarisumirangupta <kumarisumirangupta1611@gmail.com>
2025-12-02 11:38:57 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        /*//Queue Practice
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(23);
+        queue.add(1);
+        queue.add(55);
+        queue.offer(60);
+
+        System.out.println(queue.peek());
+        System.out.println(queue.poll()); //Returns null if queue is empty
+        System.out.println(queue.peek()); //Returns null if queue is empty
+        System.out.println(queue.remove()); //Returns exception if queue is empty
+        System.out.println(queue.remove());
+        System.out.println(queue.poll());
+        System.out.println(queue.element()); //Returns exception if queue is empty */
+    }
+}
