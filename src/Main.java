c661c50ef065a4a58a242f31abb1414f0f2f53c2
kumarisumirangupta <kumarisumirangupta1611@gmail.com>
2025-12-03 16:17:59 +0530

    learn set

--- src/Main.java
+++ src/Main.java
@@ -56,10 +56,10 @@ public class Main {
         ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(array));
 
         //Convert ArrayList to Array
-        Integer [] newArr = arrayList.toArray(new Integer[0]);*/
+        Integer [] newArr = arrayList.toArray(new Integer[0]);
 
         //Comparator
-        /*Car car = new Car();
+        Car car = new Car();
         System.out.println(car.getName()); //prints null default value
         System.out.println(car.getId()); // same
         System.out.println(car.getType()); //same
@@ -72,15 +72,15 @@ public class Main {
                 new Car(1,"Suv", "abcd"),
                 new Car(2, "Ostrich", "hello"),
                 new Car(3, "Hetchback", "petrol")
-        }; */
-        /*Arrays.sort(carArr, new Comparator<Car>() {
+        };
+        Arrays.sort(carArr, new Comparator<Car>() {
             @Override
             public int compare(Car obj1,  Car obj2) {
                 // if obj1 is a and obj2 is b a>b return -1 if obj1 = b then b>a return 1 and swap
                // return obj1.getType().compareTo(obj2.getType());
                 return obj2.getType().compareTo(obj1.getType());
                 //b > a = 1 swap or obj1 = a then b>a = swap obj1=b a>b return -1
-                /*he result is a negative integer if this String object lexicographically precedes the argument string.
+                the result is a negative integer if this String object lexicographically precedes the argument string.
                 The result is a positive integer if this String object lexicographically follows the argument string.
                 The result is zero if the strings are equal; compareTo returns 0
                 exactly when the equals(Object) method would return true.
@@ -94,6 +94,70 @@ public class Main {
         for (Car value : carArr) {
             System.out.println(value);
         }*/
+
+        //Array Deque
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+        //Queue behaviour
+        queue.addLast(2); //Adding in rear for empty queue tail and head = 0
+        queue.addLast(3);
+        //System.out.println(queue.peek());
+        queue.removeFirst();
+        //System.out.println(queue.peek());
+
+        //Or using add method
+        queue.add(11);//Internally calls add last
+        queue.add(12);
+        queue.remove();// Internally calls add first
+        for(Integer q : queue){
+            System.out.println(q);
+        }
+
+        //Implement Stack using Dequeu
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        stack.addFirst("top1"); //Java convention to use top as first
+        stack.addFirst("top2");
+        stack.addFirst("top3");
+        stack.removeFirst();
+        for(String q : stack){
+            System.out.println(q);
+        }
+
+
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Sumiran");
+        map.put(2, "Lokesh");
+        map.put(3, "Simran");
+
+        map.forEach((key, value)-> {
+            System.out.println("this is key "+key);
+            System.out.println("this is value "+ value);
+        });
+
+        Set<Map.Entry<Integer, String>> entries = map.entrySet();
+        Iterator<Map.Entry<Integer, String>> iterator = entries.iterator();
+
+        //Iterating through for
+        for(; iterator.hasNext(); ){
+            Map.Entry<Integer, String> next = iterator.next();
+            System.out.println(next.getKey() + next.getValue());
+        }
+
+        for(Map.Entry<Integer, String> mapEntry : entries) {
+            System.out.println("map entry way "+ mapEntry.getKey() + mapEntry.getValue());
+        }
+
+        Set<Integer> keySet = map.keySet();
+        for(Integer key : keySet) {
+            String value = map.get(key);
+            System.out.println("using key set "+ key + " "+ value);
+        }
+
+        Collection<String> values = map.values();
+        for (String val : values) {
+            System.out.println(val);
+        }
+
 
     }
 }
